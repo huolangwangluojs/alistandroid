fb5b2d335accbde3eb11be7082ed08eb52a9503f
火狼网络 <mianfeikejiwangluo@163.com>
2023-09-09 05:51:21 +0000

    update alist.sh.
    Signed-off-by: 火狼网络 <mianfeikejiwangluo@163.com>

--- alist.sh
+++ alist.sh
@@ -4,5 +4,5 @@ mkdir ./AList && cd AList
 aria2c -x 4 -s 16 https://ghproxy.com/https://github.com/Xhofe/alist/releases/latest/download/alist-linux-musl-arm64.tar.gz
 tar zxf alist-linux-musl-arm64.tar.gz
 rm -rf alist-linux-musl-arm64.tar.gz
-cd && echo "cd .termux && ./alist server">alist-server.sh && chmod 777 alist-server.sh
-sleep 0.5s && echo -e "\n 输入[1;32m./alist-server.sh[m运行\n"
+cd && echo "cd AList && ./alist server">alist-server.sh && chmod 777 alist-server.sh
+sleep 0.5s && echo -e "\n 输入./alist-server.sh运行\n"
